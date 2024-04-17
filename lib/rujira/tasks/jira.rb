9245050c6e5b648934a2874264cc109f5064d321
Andrey Semenov <i@itmage.ru>
2024-04-17 08:17:20 +0300

    iTMG: Lessword task generator

--- lib/rujira/tasks/jira.rb
+++ lib/rujira/tasks/jira.rb
@@ -15,68 +15,38 @@ module Rujira
       end
 
       def define
-        whoami
-        url
-        server_info
-        api
-      end
-
-      def whoami
-        name = __method__
-        desc 'Request my real name in Jira'
-
-        Rake::Task[name].clear if Rake::Task.task_defined?(name)
-        namespace :jira do
-          task name do
-            puts Rujira::Api::Myself.get.name
+        generate 'whoami' do
+          puts Rujira::Api::Myself.get.name
+        end
+        generate 'url' do
+          puts Rujira::Configuration.url
+        end
+        generate 'server_info' do
+          puts Rujira::Api::ServerInfo.get.data.to_json
+        end
+        generate 'search' do
+          options = {}
+          o = OptionParser.new
+          o.banner = "Usage: rake jira:task:search -- '[options]'"
+          o.on('-q JQL', '--jql JQL') do |jql|
+            options[:jql] = jql
           end
+          args = o.order!(ARGV) {}
+          o.parse!(args)
+
+          result = Rujira::Api::Search.get jql: options[:jql]
+          result.each { |i| puts JSON.pretty_generate(i.data) }
         end
       end
 
-      def url
-        name = __method__
-        desc 'Request server url from Jira'
+      def generate(name, &block)
+        fullname = "jira:#{name}"
+        desc "Run #{fullname}"
 
-        Rake::Task[name].clear if Rake::Task.task_defined?(name)
+        Rake::Task[fullname].clear if Rake::Task.task_defined?(fullname)
         namespace :jira do
           task name do
-            puts Rujira::Configuration.url
-          end
-        end
-      end
-
-      def server_info
-        name = __method__
-        desc 'Request server information from Jira'
-
-        Rake::Task[name].clear if Rake::Task.task_defined?(name)
-        namespace :jira do
-          task name do
-            puts Rujira::Api::ServerInfo.get.data.to_json
-          end
-        end
-      end
-
-      def api
-        name = 'jira:task:search'
-        desc 'Example Jira command'
-
-        Rake::Task[name].clear if Rake::Task.task_defined?(name)
-        namespace :jira do
-          namespace :task do
-            task :search do
-              options = {}
-              o = OptionParser.new
-              o.banner = "Usage: rake jira:task:search -- '[options]'"
-              o.on('-q JQL', '--jql JQL') do |jql|
-                options[:jql] = jql
-              end
-              args = o.order!(ARGV) {}
-              o.parse!(args)
-
-              result = Rujira::Api::Search.get jql: options[:jql]
-              result.each { |i| puts JSON.pretty_generate(i.data) }
-            end
+            instance_eval(&block)
           end
         end
       end
