385f7a55bff3a5235b0fb97bebcf02c957c2d7a5
Andrey Semenov <i@itmage.ru>
2024-08-25 12:00:50 +0300

    iTMG: Rewrite tasks for gem

--- lib/rujira/api/search.rb
+++ lib/rujira/api/search.rb
@@ -3,7 +3,7 @@
 module Rujira
   module Api
     # TODO
-    # https://docs.atlassian.com/software/jira/docs/api/REST/8.17.1/#api/2/search
+    # https://docs.atlassian.com/software/jira/docs/api/REST/9.17.0/#api/2/search
     class Search < Item
       def self.get(&block)
         entity = Entity.build do
--- lib/rujira/tasks/jira.rb
+++ lib/rujira/tasks/jira.rb
@@ -30,84 +30,87 @@ module Rujira
 
       # rubocop:disable Metrics/AbcSize
       # rubocop:disable Metrics/MethodLength
+      # rubocop:disable Metrics/BlockLength
       def define
-        generate 'whoami' do
-          puts Rujira::Api::Myself.get.name
-        end
-        generate 'url' do
-          puts Rujira::Configuration.url
-        end
-        generate 'server_info' do
-          puts Rujira::Api::ServerInfo.get.data.to_json
-        end
-        generate 'create' do
-          parser do
-            @parser.banner = "Usage: rake jira:task:create -- '[options]'"
-            @parser.on('-p PROJECT', '--project=PROJECT') { |project| @options[:project] = project.strip }
-            @parser.on('-s SUMMARY', '--summary=SUMMARY') { |summary| @options[:summary] = summary.strip }
-            @parser.on('-d DESCRIPTION', '--description=DESCRIPTION') do |description|
-              @options[:description] = description.strip
+        namespace :jira do
+          desc 'Test connection by getting username'
+          task :whoami do
+            puts Rujira::Api::Myself.get.name
+          end
+          desc 'Test connection by getting url'
+          task :url do
+            puts Rujira::Configuration.url
+          end
+          desc 'Test connection by getting server information'
+          task :server_info do
+            puts Rujira::Api::ServerInfo.get.data.to_json
+          end
+          namespace :task do
+            desc 'Create a task'
+            task :create do
+              parser do
+                @parser.banner = "Usage: rake jira:task:create -- '[options]'"
+                @parser.on('-p PROJECT', '--project=PROJECT') { |project| @options[:project] = project.strip }
+                @parser.on('-s SUMMARY', '--summary=SUMMARY') { |summary| @options[:summary] = summary.strip }
+                @parser.on('-d DESCRIPTION', '--description=DESCRIPTION') do |description|
+                  @options[:description] = description.strip
+                end
+                @parser.on('-i ISSUETYPE', '--issuetype=ISSUETYPE') do |issuetype|
+                  @options[:issuetype] = issuetype.strip
+                end
+              end
+
+              options = @options
+              result = Rujira::Api::Issue.create do
+                data fields: {
+                  project: { key: options[:project] },
+                  summary: options[:summary],
+                  issuetype: { name: options[:issuetype] },
+                  description: options[:description]
+                }
+              end
+              url = Rujira::Configuration.url
+              puts "// A new task been posted, check it out at #{url}/browse/#{result.data['key']}"
             end
-            @parser.on('-i ISSUETYPE', '--issuetype=ISSUETYPE') { |issuetype| @options[:issuetype] = issuetype.strip }
-          end
+            desc 'Search task by fields'
+            task :search do
+              parser do
+                @parser.banner = "Usage: rake jira:task:search -- '[options]'"
+                @parser.on('-q JQL', '--jql=JQL') { |jql| @options[:jql] = jql }
+              end
 
-          options = @options
-          result = Rujira::Api::Issue.create do
-            data fields: {
-              project: { key: options[:project] },
-              summary: options[:summary],
-              issuetype: { name: options[:issuetype] },
-              description: options[:description]
-            }
-          end
-          url = Rujira::Configuration.url
-          puts "// A new task been posted, check it out at #{url}/browse/#{result.data['key']}"
-        end
-        generate 'search' do
-          parser do
-            @parser.banner = "Usage: rake jira:task:search -- '[options]'"
-            @parser.on('-q JQL', '--jql=JQL') { |jql| @options[:jql] = jql }
-          end
+              options = @options
+              result = Rujira::Api::Search.get do
+                data jql: options[:jql]
+              end
+              result.iter.each { |i| puts JSON.pretty_generate(i.data) }
+            end
+            desc 'Delete task'
+            task :delete do
+              parser do
+                @parser.banner = "Usage: rake jira:task:delete -- '[options]'"
+                @parser.on('-i ID', '--issue=ID') { |id| @options[:id] = id }
+              end
 
-          options = @options
-          result = Rujira::Api::Search.get do
-            data jql: options[:jql]
-          end
-          result.iter.each { |i| puts JSON.pretty_generate(i.data) }
-        end
-        generate 'delete' do
-          parser do
-            @parser.banner = "Usage: rake jira:task:delete -- '[options]'"
-            @parser.on('-i ID', '--issue=ID') { |id| @options[:id] = id }
-          end
+              Rujira::Api::Issue.del @options[:id]
+            end
+            desc 'Example usage attaching in task'
+            task :attach do
+              parser do
+                @parser.banner = "Usage: rake jira:task:attach -- '[options]'"
+                @parser.on('-f FILE', '--file=FILE') { |file| @options[:file] = file }
+                @parser.on('-i ID', '--issue=ID') { |id| @options[:id] = id }
+              end
 
-          Rujira::Api::Issue.del @options[:id]
-        end
-        generate 'attach' do
-          parser do
-            @parser.banner = "Usage: rake jira:task:attach -- '[options]'"
-            @parser.on('-f FILE', '--file=FILE') { |file| @options[:file] = file }
-            @parser.on('-i ID', '--issue=ID') { |id| @options[:id] = id }
+              result = Rujira::Api::Issue.attachments @options[:id], @options[:file]
+              puts JSON.pretty_generate(result.data)
+            end
           end
-
-          result = Rujira::Api::Issue.attachments @options[:id], @options[:file]
-          puts JSON.pretty_generate(result.data)
         end
       end
+      # rubocop:enable Metrics/BlockLength
       # rubocop:enable Metrics/AbcSize
       # rubocop:enable Metrics/MethodLength
-
-      def generate(name, &block)
-        fullname = "jira:#{name}"
-        desc "Run #{fullname}"
-
-        Rake::Task[fullname].clear if Rake::Task.task_defined?(fullname)
-        namespace :jira do
-          task name do
-            instance_eval(&block)
-          end
-        end
-      end
     end
   end
 end
