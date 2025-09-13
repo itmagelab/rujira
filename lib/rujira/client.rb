a2da981144a438d55d7e5b263760148300a26ac5
Andrey Semenov <i@itmage.ru>
2025-09-13 15:29:30 +0300

    doc: added comments from ai #2

--- lib/rujira/client.rb
+++ lib/rujira/client.rb
@@ -1,17 +1,40 @@
 # frozen_string_literal: true
 
 module Rujira
-  # TODO: add docs
-  # Some description
+  # Main Jira API client.
+  # Provides access to Jira resources via method_missing delegation to API classes.
+  #
+  # Example:
+  #   client = Rujira::Client.new("https://jira.example.com")
+  #   client.issue.get("TEST-123")
+  #
   class Client
+    # @return [Rujira::Request] The current request object being configured
     attr_accessor :request
 
+    # Initializes a new Jira client.
+    #
+    # @param [String] url Base URL of the Jira instance.
+    # @param [Boolean] debug Whether to enable debug logging. Can also be set via ENV['RUJIRA_DEBUG'].
+    #
+    # @example Initialize client
+    #   client = Rujira::Client.new("https://jira.example.com", debug: true)
+    #
     def initialize(url, debug: false)
       @uri = URI(url)
       @debug = ENV.fetch('RUJIRA_DEBUG', debug.to_s) == 'true'
       @request = Request.new
     end
 
+    # Dynamically instantiates the appropriate API resource class.
+    #
+    # @param [Symbol] method_name Name of the API resource (e.g., :issue, :project)
+    # @param [...] args Arguments passed to the resource class constructor
+    # @return [Object] An instance of the corresponding API resource class
+    #
+    # @example Access an API resource
+    #   client.issue.get("TEST-123")
+    #
     def method_missing(method_name, ...)
       resource_class = Rujira::Api.const_get(method_name.to_s)
       resource_class.new(self, ...)
@@ -19,19 +42,33 @@ module Rujira
       super
     end
 
+    # Options for Faraday connection.
+    #
+    # @return [Hash] Options including URL, headers, and params
+    #
     def options
       {
         url: @uri,
         headers: @request.headers,
         params: @request.params
-
       }
     end
 
+    # Checks if a resource class exists for method_missing.
+    #
+    # @param [Symbol] method_name Method name to check
+    # @param [Boolean] include_private Include private methods
+    # @return [Boolean] true if the resource exists, false otherwise
+    #
     def respond_to_missing?(method_name, include_private = false)
       Rujira::Api.const_defined?(method_name.to_s) || super
     end
 
+    # Executes the configured request.
+    #
+    # @return [Object] The API response body if successful
+    # @raise [RuntimeError] If the request fails or method is unsupported
+    #
     def dispatch # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
       unless %i[get delete head post put patch].include?(@request.method)
         raise "method #{@request.method} not supported"
@@ -49,6 +86,10 @@ module Rujira
       end
     end
 
+    # Builds the Faraday connection for HTTP requests.
+    #
+    # @return [Faraday::Connection] Configured Faraday connection
+    #
     def connection
       Faraday.new(options) do |builder|
         builder.request :authorization, *@request.authorization if @request.authorization
@@ -60,10 +101,20 @@ module Rujira
       end
     end
 
+    # Prepares a file for multipart upload.
+    #
+    # @param [String] path Path to the file
+    # @return [Faraday::Multipart::FilePart] File ready for upload
+    #
     def file(path)
       Faraday::Multipart::FilePart.new(path, 'multipart/form-data')
     end
 
+    # Generates mock JSON files for API responses (useful for caching or testing).
+    #
+    # @param [Object] res API response object
+    # @return [void]
+    #
     def generate_mocks(res)
       path = res.to_hash[:url].path
       cache_path = File.join('.cache', path)
--- lib/rujira/request.rb
+++ lib/rujira/request.rb
@@ -3,11 +3,17 @@
 require 'fileutils'
 
 module Rujira
-  # TODO: add docs
-  # Some description
+  # Represents an HTTP request configuration for Jira API calls.
+  # Used internally by Rujira::Client to build requests with headers, params, payloads, and authorization.
+  #
   class Request
-    attr_reader :authorization, :options
+    # @return [Array, nil] Authorization information, e.g., Bearer token or basic auth credentials
+    attr_reader :authorization
+    # @return [Hash] Additional options for the request (currently unused)
+    attr_reader :options
 
+    # Initializes a new request object with default values.
+    #
     def initialize
       @token = ENV['RUJIRA_TOKEN'] if ENV.include?('RUJIRA_TOKEN')
       @method = :get
@@ -19,53 +25,104 @@ module Rujira
       @path = nil
     end
 
+    # Evaluates a block in the context of the request for configuration.
+    #
+    # @yield [self] Block used to set path, headers, params, method, etc.
+    # @return [Request] Returns self for chaining.
+    #
+    # @example Configure a request
+    #   request.builder do
+    #     path 'issue/TEST-123'
+    #     method :get
+    #     headers 'X-Custom-Header': 'value'
+    #   end
+    #
     def builder(&block)
       return self unless block_given?
 
       instance_eval(&block)
       self
     end
-
     alias build builder
 
+    # Sets the base REST path for the request.
+    #
+    # @param [String] path The base path (e.g., 'rest/api/2' or 'rest/agile/1.0')
+    # @return [void]
+    #
     def rest_base(path)
       @rest_base_path = path
     end
 
+    # Gets or sets query parameters for the request.
+    #
+    # @param [Hash, nil] params Optional parameters to set.
+    # @return [Hash] The current parameters.
+    #
     def params(params = nil)
       return @params if params.nil?
 
       @params = params
     end
 
+    # Gets or sets HTTP headers for the request.
+    #
+    # @param [Hash, nil] headers Optional headers to set.
+    # @return [Hash] The current headers.
+    #
     def headers(headers = nil)
       return @headers if headers.nil?
 
       @headers = headers
     end
 
+    # Gets or sets the HTTP method for the request.
+    #
+    # @param [Symbol, nil] method Optional HTTP method (:get, :post, etc.)
+    # @return [Symbol] The current method.
+    #
     def method(method = nil)
       return @method if method.nil?
 
       @method = method
     end
 
+    # Sets Bearer token authorization.
+    #
+    # @param [String] token The token to use for authorization.
+    # @return [void]
+    #
     def bearer(token)
       @authorization = 'Bearer', -> { token }
     end
 
+    # Sets basic authentication.
+    #
+    # @param [String] username The username.
+    # @param [String] password The password.
+    # @return [void]
+    #
     def basic(username, password)
       @authorization = :basic, username, password
     end
 
+    # Gets or sets the request path (appended to rest_base_path).
+    #
+    # @param [String, nil] path The relative path to set.
+    # @return [String] The full path combining rest_base_path and the given path.
+    #
     def path(path = nil)
       return @path if path.nil?
 
       @path = "#{@rest_base_path}/#{path}"
-
       @path
     end
 
+    # Gets or sets the request payload (body for POST/PUT/PATCH requests).
+    #
+    # @param [Object, nil] payload The payload to set.
+    # @return [Object] The current payload.
+    #
     def payload(payload = nil)
       return @payload if payload.nil?
 
