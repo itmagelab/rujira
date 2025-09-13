6d6a1c1f6291d0c5fdc2ab588d6c5ea835c5c862
Andrey Semenov <i@itmage.ru>
2025-09-13 15:31:07 +0300

    doc: added comments from ai #3

--- lib/rujira.rb
+++ lib/rujira.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
+# Load Rake tasks if Rake is defined
 Dir[File.join(__dir__, 'rujira', 'tasks', '*.rake')].each { |r| load r } if defined?(Rake)
 
 require 'faraday'
 require 'faraday/multipart'
 require 'json'
+
 require_relative 'rujira/error'
 require_relative 'rujira/version'
 require_relative 'rujira/request'
@@ -21,11 +23,27 @@ require_relative 'rujira/api/dashboard'
 require_relative 'rujira/api/board'
 require_relative 'rujira/api/sprint'
 
-# TODO: add docs
-# Some description
+# Main Rujira module.
+# Serves as the namespace for the Jira SDK.
+#
+# Provides utility methods and custom error handling.
+#
 module Rujira
+  # Custom error class for Rujira SDK
+  #
+  # @example
+  #   raise Rujira::Error, "Something went wrong"
+  #
   class Error < StandardError; end
 
+  # Checks if an environment variable is truthy.
+  #
+  # @param [String] var The name of the environment variable
+  # @return [Boolean] true if the value is 'true', '1', or 'yes' (case-insensitive), false otherwise
+  #
+  # @example Check if debug mode is enabled
+  #   Rujira.env_var?('RUJIRA_DEBUG')
+  #
   def self.env_var?(var)
     %w[true 1 yes].include?(ENV[var]&.downcase)
   end
